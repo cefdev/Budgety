d8ce0a467662952604d01b95c53c3001dd2cd128
Moncef <moncef.koumila@gmail.com>
2020-01-07 01:07:12 +0000

    Create Budget and UI and Controller modules

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+ * Budget Controller
+*/
+const budgetController = (function(){
+
+})()
+
+
+/*
+ * User Interface Controller
+*/
+const UIController = (function(){
+
+})()
+
+
+/*
+ * The Controller
+*/
+const controller = (function(budgetCtrl, UICtrl){
+
+})(budgetController, UIController)
+
